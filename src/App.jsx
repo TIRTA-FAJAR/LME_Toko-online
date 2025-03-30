1840f9c776a479a3daaf8af54c7867e8a268eff5
JAR <incsinkator@gmail.com>
2025-03-30 11:04:41 +0700

    halaman menu - ubah judul

--- src/App.jsx
+++ src/App.jsx
@@ -27,59 +27,59 @@ function App() {
               <Link to="/about">About</Link>
             </li>
             <li className='app-login'>
-             <Link to="/login">Login</Link>
+              <Link to="/login">Login</Link>
             </li>
           </ul>
         </div>
       </div>
 
-     {/* Definisi Routing */}
+      {/* Definisi Routing */}
       <Routes>
-          <Route path="/" element={<Menu />}/>  
-          <Route path="/menu" element={<Menu />}/>  
-          <Route path="/about" element={<About />}/>
-          <Route path="/login" element={<LoginPage />}/>  
-          <Route path="/register" element={<RegisterPage />}/>  
-        </Routes>
+        <Route path="/" element={<Menu />} />
+        <Route path="/menu" element={<Menu />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+      </Routes>
 
       {/* <!-- FOTTER --> */}
-    <div class="footer">
+      <div class="footer">
         <div class="pertama">
-            <img src="../src/assets/img/LME.png" alt="LookMyEys" title="LookMyEys"/>
-            <p>COPYRIGHT &copy; CREATED <br/> BY LookMyEyes</p>
+          <img src="../src/assets/img/LME.png" alt="LookMyEys" title="LookMyEys" />
+          <p>COPYRIGHT &copy; CREATED <br /> BY LookMyEyes</p>
         </div>
-        <div class="isi">            
-            <div class="coment">
-              <p>Terima kasih telah mengunjungi situs kami. Kami siap melayani Anda!</p>
-              <p>Beli produk asli lokal berkualitas internasional hanya di LME!</p>
-              <p>Bila ada sesuatu yang ingin di sampaikan, kirim pesan pada sosial media kami yang tertera.</p>
+        <div class="isi">
+          <div class="coment">
+            <p>Terima kasih telah mengunjungi situs kami. Kami siap melayani Anda!</p>
+            <p>Beli produk asli lokal berkualitas internasional hanya di LME!</p>
+            <p>Bila ada sesuatu yang ingin di sampaikan, kirim pesan pada sosial media kami yang tertera.</p>
+          </div>
+          <div class="sosmed">
+            <div class="instagram">
+              <a href=""><img src="../src/assets/img/sosmed/instagram.png" alt="" /></a>
+              <p>instagram</p>
             </div>
-            <div class="sosmed">
-                <div class="instagram">
-                  <a href=""><img src="../src/assets/img/sosmed/instagram.png" alt="" /></a>
-                  <p>instagram</p>
-                </div>
-                <div class="facebok">
-                  <a href=""><img src="../src/assets/img/sosmed/facebook.png" alt="" /></a>
-                  <p>facebook</p>
-                </div>
-                <div class="x">
-                  <a href=""><img src="../src/assets/img/sosmed/twitter.png" alt=""/></a>
-                  <p>twiter</p>
-                </div>
-                <div class="link">
-                  <a href=""><img src="../src/assets/img/sosmed/linkedin.png" alt=""/></a>
-                  <p>linkedin</p>
-                </div>
+            <div class="facebok">
+              <a href=""><img src="../src/assets/img/sosmed/facebook.png" alt="" /></a>
+              <p>facebook</p>
             </div>
+            <div class="x">
+              <a href=""><img src="../src/assets/img/sosmed/twitter.png" alt="" /></a>
+              <p>twiter</p>
+            </div>
+            <div class="link">
+              <a href=""><img src="../src/assets/img/sosmed/linkedin.png" alt="" /></a>
+              <p>linkedin</p>
+            </div>
+          </div>
         </div>
-    </div>
-    {/* <!-- END FOTTER --> */}
+      </div>
+      {/* <!-- END FOTTER --> */}
 
 
     </Router>
 
-    
+
   );
 }
 
--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -4,34 +4,34 @@ import buku from '../assets/img/produk/buku.png'
 import toli from '../assets/img/produk/totbag.png'
 
 const Menu = () => {
-  return ( 
+  return (
     <>
-    <div class="menu-container">
+      <div class="menu-container">
         <div class="menu-judul">
-            <h1>Welcome IndoPride!</h1>
+          <h1>Welcome SEMUANYA</h1>
         </div>
         <div class="content">
-            <div class="content-isi">
-                <img src={buku} alt="" />
-                <p>"Ada Nama mu Dalam Do'a"</p><br />
-                <p>Beli lah buku "Ada Nama mu Dalam Do'a" edisi terbatas, 
-                    buku ini cocok buat kalian yang lagi mengincar seorang wanita
-                    tetapi kamu merasa tidak cocok dikarena terbas dengan ketakwaan.</p><br />
-                    <p>Price : Rp80.000</p>
-                    <p>Owner : LME</p>
-                    <p>Karya : -</p>
-            </div>
-            <div class="content-isi">
+          <div class="content-isi">
+            <img src={buku} alt="" />
+            <p>"Ada Nama mu Dalam Do'a"</p><br />
+            <p>Beli lah buku "Ada Nama mu Dalam Do'a" edisi terbatas,
+              buku ini cocok buat kalian yang lagi mengincar seorang wanita
+              tetapi kamu merasa tidak cocok dikarena terbas dengan ketakwaan.</p><br />
+            <p>Price : Rp80.000</p>
+            <p>Owner : LME</p>
+            <p>Karya : -</p>
+          </div>
+          <div class="content-isi">
 
-            </div>
-            <div class="content-isi">
+          </div>
+          <div class="content-isi">
 
-            </div>
-            <div class="content-isi">
+          </div>
+          <div class="content-isi">
 
-            </div>
+          </div>
         </div>
-    </div>
+      </div>
     </>
   );
 
